27229b245839d35c3277f67d80862eb17c8fcfbd
Angelios <93891389+AngeliosX@users.noreply.github.com>
2022-07-06 08:10:29 +0500

    Update DayController.java

--- restful/controller/DayController.java
+++ restful/controller/DayController.java
@@ -1,1 +1,22 @@
+package com.hw4.restful.controller;
 
+import com.hw4.restful.entity.Day;
+import com.hw4.restful.service.DayService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class DayController {
+
+    private final DayService dayService;
+
+    public DayController(DayService dayService) {
+        this.dayService = dayService;
+    }
+
+    @RequestMapping("/day")
+    public Day getDay() {
+        return dayService.getDay();
+    }
+}
