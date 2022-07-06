309c7ea5d67a9f2cd2c14d9ea89fe043bc0c7542
Angelios <93891389+AngeliosX@users.noreply.github.com>
2022-07-06 08:11:22 +0500

    Create Day.java

---
+++ restful/entity/Day.java
@@ -0,0 +1,16 @@
+package com.hw4.restful.entity;
+
+public class Day {
+    private String date;
+
+    public Day() {
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
