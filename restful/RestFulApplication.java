882ccc14b9d80ef52489d228707103643e03a12c
Angelios <93891389+AngeliosX@users.noreply.github.com>
2022-07-06 08:14:53 +0500

    Create RestFulApplication.java

---
+++ restful/RestFulApplication.java
@@ -0,0 +1,12 @@
+package com.hw4.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestFulApplication.class, args);
+    }
+}
